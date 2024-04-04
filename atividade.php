3980d4e04efcd4ee2827e0e9625fa7aab20a3350
Lucas2667 <161774548+Lucas2667@users.noreply.github.com>
2024-04-04 20:28:10 -0300

    Add files via upload

---
+++ atividade.php
@@ -0,0 +1,8 @@
+<?php
+
+$multiplicador = readline("Escreva um número de 1 à 10: ");
+ for ($i = 0; $i<= 10; $i++) {
+     echo $multiplicador . "x" . "$i" .
+         "=" . $i * $multiplicador . PHP_EOL;
+ }
+ ?>
