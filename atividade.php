7ec2eb89357b9818235eec4ccb1cc96325c125b7
Lucas2667 <161774548+Lucas2667@users.noreply.github.com>
2024-04-12 12:25:39 -0300

    atividade.php

--- atividade.php
+++ atividade.php
@@ -1,8 +1,6 @@
 <?php
-
-$multiplicador = readline("Escreva um número de 1 à 10: ");
- for ($i = 0; $i<= 10; $i++) {
-     echo $multiplicador . "x" . "$i" .
-         "=" . $i * $multiplicador . PHP_EOL;
- }
- ?>
+    $multiplicador = readline("Escreva um número de 1 à 10: ");
+    for($i = 0; $i<=10; $i++){
+        echo $multiplicador . "x" . "$i" . "=".$i*$multiplicador.PHP_EOL;
+    }
+?>
